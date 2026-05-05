1706568cdbe2363d75d23465a74052a39b5a926b
Seokwon Choi <seokwon0106@yonsei.ac.kr>
2026-05-04 21:04:16 -0700

    Updates

---
+++ _next/static/JIl392g0kz0b1lOZU1kqY/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/seokwon_choi_profile-main//_next/:path+",
+        "destination": "/seokwon_choi_profile-main/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
